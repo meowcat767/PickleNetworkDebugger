b00773393263dc9630643f8131e8429f6f7696d6
Ben <meowcat@meowcat.site>
2026-04-22 16:26:01 +0100

    no more shonk

--- src/main/kotlin/site/meowcat/pkn/Launcher.kt
+++ src/main/kotlin/site/meowcat/pkn/Launcher.kt
@@ -1,41 +1,28 @@
 package site.meowcat.pkn
 
-import javafx.application.Application
-import javafx.scene.Scene
-import javafx.scene.control.TextArea
-import javafx.stage.Stage
-import kotlin.concurrent.thread
-
-class App : Application() {
-
-    override fun start(stage: Stage) {
-        val output = TextArea()
-        output.isEditable = false
-
-        stage.title = "Pickle Network Debugger"
-        stage.scene = Scene(output, 800.0, 500.0)
-        stage.show()
-
-        thread {
-            val process = ProcessBuilder(
-                "tshark",
-                "-i", "any",
-                "-T", "fields",
-                "-e", "ip.src",
-                "-e", "ip.dst",
-                "-e", "_ws.col.Protocol",
-                "-c", "10"
-            ).start()
-
-            process.inputStream.bufferedReader().forEachLine {
-                javafx.application.Platform.runLater {
-                    output.appendText("$it\n")
-                }
-            }
-        }
-    }
-}
+import org.pcap4j.core.Pcaps
+import org.pcap4j.core.PcapNetworkInterface
+import org.pcap4j.core.RawPacketListener
 
 fun main() {
-    Application.launch(App::class.java)
+    val nifs = Pcaps.findAllDevs()
+
+    val nif = nifs.firstOrNull() ?: run {
+        println("No interfaces found")
+        return
+    }
+
+    println("Using: ${nif.name}")
+
+    val handle = nif.openLive(
+        65536,
+        PcapNetworkInterface.PromiscuousMode.PROMISCUOUS,
+        10
+    )
+
+    handle.loop(10, RawPacketListener { packet ->
+        println("Packet length: ${packet.size}")
+    })
+
+    handle.close()
 }
