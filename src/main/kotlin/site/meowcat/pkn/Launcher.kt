6343099deb4f36abd902cb87384069f304d242dc
Ben <meowcat@meowcat.site>
2026-04-22 16:05:47 +0100

    basic window

--- src/main/kotlin/site/meowcat/pkn/Launcher.kt
+++ src/main/kotlin/site/meowcat/pkn/Launcher.kt
@@ -1,19 +1,41 @@
 package site.meowcat.pkn
 
 import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.TextArea
+import javafx.stage.Stage
+import kotlin.concurrent.thread
+
+class App : Application() {
+
+    override fun start(stage: Stage) {
+        val output = TextArea()
+        output.isEditable = false
+
+        stage.title = "Pickle Network Debugger"
+        stage.scene = Scene(output, 800.0, 500.0)
+        stage.show()
+
+        thread {
+            val process = ProcessBuilder(
+                "tshark",
+                "-i", "any",
+                "-T", "fields",
+                "-e", "ip.src",
+                "-e", "ip.dst",
+                "-e", "_ws.col.Protocol",
+                "-c", "10"
+            ).start()
+
+            process.inputStream.bufferedReader().forEachLine {
+                javafx.application.Platform.runLater {
+                    output.appendText("$it\n")
+                }
+            }
+        }
+    }
+}
 
 fun main() {
-    val process = ProcessBuilder(
-        "tshark",
-        "-i", "any",
-        "-T", "fields",
-        "-e", "ip.src",
-        "-e", "ip.dst",
-        "-e", "_ws.col.Protocol",
-        "-c", "10"
-    ).start()
-
-    process.inputStream.bufferedReader().forEachLine {
-        println(it)
-    }
+    Application.launch(App::class.java)
 }
